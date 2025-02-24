0626c4805f3e4714d482be7382629f7f42a69dd2
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-02-24 18:32:40 +0600

    file added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <h1 className="m-2 text-4xl">Maa</h1>
+      </div>
+
+      <div className="m-8 flex items-center justify-center gap-4 text-2x1">
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,26 @@
+import { PROJECTS } from "../constants";
+
+const Projects = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="my-20 text-center text-4x1">Projects</h1>
+      <div>
+        {PROJECTS.map((project, index) => (
+          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+            <div className="w-full lg:w-1/4">
+              <img
+                src={project.image}
+                width={150}
+                height={150}
+                alt={project.title}
+                className="mb-6 rounded"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
