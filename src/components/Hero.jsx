bc7591d20f876f2865b001f675b3fa081ed15272
mohsinahmedarfat <mohsinarfat53@gamil.com>
2025-02-27 16:40:39 +0600

    add new profile images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import { HERO_CONTENT } from "../constants";
 import { motion } from "framer-motion";
-import profilePic from "../assets/mohsinAhmedArfat.png";
 
 // func (return an object): make motion props shorter in tags for better readability
 const container = (delay) => ({
@@ -41,16 +40,18 @@ const Hero = () => {
           </div>
         </div>
         {/* image */}
-        <div className="w-full lg:w-1/2">
-          <div className="flex justify-center">
+        <div className="w-full lg:w-1/2 relative">
+          <div className="flex justify-center relative">
             <motion.img
               initial={{ x: +100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
               transition={{ duration: 1, delay: 1.2 }}
-              className="w-[85%]"
-              src={profilePic}
+              className="w-3/4 rounded-2xl relative"
+              src="https://imgur.com/lVerHjz.png"
               alt="Mohsin Ahmed Arfat"
             />
+            {/* Gradient Overlay */}
+            <div className="absolute inset-y-0 w-3/4 rounded-2xl bg-gradient-to-t from-black/60 via-black/30 to-transparent"></div>
           </div>
         </div>
       </div>
